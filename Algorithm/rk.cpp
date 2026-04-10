b967a213fe04e6d31cd915cafef4e4b56ac8a865
hgdat2534 <hgdat2534@clc.fitus.edu.vn>
2026-04-10 23:45:04 +0700

    Fixed negative hash

--- Algorithm/rk.cpp
+++ Algorithm/rk.cpp
@@ -20,34 +20,33 @@ vector<pair<int, int>> rk(string toSearch, string target, string& name, int& cmp
 
     long long highOrder = 1;
     for (int i = 0; i < TARGET_LEN - 1; ++i) {
-        highOrder *= radix;
+        highOrder = (highOrder * radix) % q;
     }
-    highOrder = highOrder % q;
+
 
     long long targetNum = 0;
     long long curSearch = 0;
 
     // hashing m first chars in toSearch and hashing target
     for (int i = 0; i < TARGET_LEN; ++i) {
-        targetNum = (radix * targetNum + target[i]) % q;
-        curSearch = (radix * curSearch + toSearch[i]) % q;
+        targetNum = (radix * targetNum + (unsigned char)target[i]) % q;
+        curSearch = (radix * curSearch + (unsigned char)toSearch[i]) % q;
     }
 
+
+    
     // searching process
     for (int i = 0; i <= SEARCH_LEN - TARGET_LEN; ++i) {
-        // check for equal hash
         if (targetNum == curSearch) {
-            // check individual chars
             if (checkString(toSearch, target, i, TARGET_LEN, cmp_cnt)) {
                 res.push_back({i, i + TARGET_LEN - 1});
             }
         }
-
+    
         if (i < SEARCH_LEN - TARGET_LEN) {
-            curSearch = (radix * (curSearch - (toSearch[i]) * highOrder) + toSearch[i + TARGET_LEN]) % q;
+            curSearch = (curSearch - (toSearch[i] * highOrder) % q + q) % q;
+            curSearch = (curSearch * radix + (unsigned char)toSearch[i + TARGET_LEN]) % q;
         }
-        
-        if (curSearch < 0) curSearch += q;
     }
 
     return res;
